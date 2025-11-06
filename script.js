e67d9e908740adb464f42b99d510df8841f62115
SarbBhangu <Bhangu513@gmail.com>
2025-11-06 15:28:35 -0500

    created local storage

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ const usernameError = document.getElementById('usernameError');
 const emailError = document.getElementById('emailError');
 const passwordError = document.getElementById('passwordError');
 const confirmPasswordError = document.getElementById('confirmPasswordError');
+
+document.addEventListener('DOMContentLoaded' , ()=> {
+    const savedUsername = localStorage.getItem('username');
+
+    if (savedUsername) {
+        username.value = savedUsername;
+        console.log("Username loaded from localStorage:", savedUsername)
+    } else {
+        console.log("No username found in localStorage yet.");
+    }
+})
